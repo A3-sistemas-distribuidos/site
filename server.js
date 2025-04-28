a9675f29d51958ed4d210917c74cc2a87b60c5fd
Rafael Rangel <rafael.profissional011@gmail.com>
2025-04-28 11:53:51 -0300

    premeira implementacao

--- server.js
+++ server.js
@@ -34,14 +34,46 @@ app.post('/postar_reserva', async (req, res) => {
     if (error) throw error;
 
     } catch (error) {
-        console.error('Erro ao criar questão:', error);
+        console.error('Erro ao criar reserva:', error);
 
         res.status(500).json({
-            mensagem: 'Error ao criar questão',
+            mensagem: 'Error ao criar reserva',
             detalhe: error.message || 'Error desconhecido',
             supabase: error.details || null})
     }
 });
+
+app.get('/mostrar_reservas_nao_confirmadas', async (req, res) => {
+    try {
+        const {mesa} = req.body;
+        const status = null;
+        let query = supabase.from('reservas').select(`
+            id,
+            nome_responsavel,
+            data_reserva,
+            hora,
+            mesa,
+            qtd_pessoas,
+            status,
+            `);
+        
+        if (mesa) query = query.eq('mesa', mesa);
+        query = query.eq('satus', status);
+
+        const {data, error} = await query;
+
+        if (error) throw error;
+
+        res.json(data);
+    } catch {
+        console.error('Erro ao consultar reserva:', error);
+
+        res.status(500).json({
+            mensagem: 'Error ao consultar reserva',
+            detalhe: error.message || 'Error desconhecido',
+            supabase: error.details || null})
+    }
+})
 
 
 app.use(express.static("public"));
